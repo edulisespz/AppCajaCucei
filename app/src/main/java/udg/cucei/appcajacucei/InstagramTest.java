16b589460357cd2686aaba6a0717cd3055267958
adolfo <adolfomichelesparza@gmail.com>
2016-03-15 17:02:14 -0600

    Instagram test

--- app/src/main/java/udg/cucei/appcajacucei/InstagramTest.java
+++ app/src/main/java/udg/cucei/appcajacucei/InstagramTest.java
@@ -1,26 +1,46 @@
 package udg.cucei.appcajacucei;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.File;
 
 /**
  * Created by adolfo on 8/03/16.
  */
-public class InstagramTest extends AppCompatActivity {
-    private void onCreate(){
-        String type = "image/*";
-        String filename = "/myPhoto.jpg";
-        String mediaPath = Environment.getExternalStorageDirectory() + filename;
+public class InstagramTest extends Activity implements Runnable {
 
-        createInstagramIntent(type, mediaPath);
+    String type = "image/*";
+    String filename = "/myPhoto.jpg";
+    String mediaPath = Environment.getExternalStorageDirectory() + filename;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instagram_test_layout);
     }
 
-    private void createInstagramIntent(String type, String mediaPath){
+    public void compartirInstagram(View buttonSource) {
+        new Thread(this).start();
+    }
+
+    public void buscarImagen(View buttonSource) {
+        String picLocation = getIntent().getStringExtra("PICTURE_LOCATION");
+
+        // Instantiate the ImageView object:
+        ImageView imageViewer = (ImageView) findViewById(R.id.imageView);
+
+        // Convert the Uri string into a usable Uri:
+        Uri temp = Uri.parse(picLocation);
+        imageViewer.setImageURI(temp);
+    }
+
+    private void createInstagramIntent(String type, String mediaPath) {
 
         // Create the new Intent using the 'Send' action.
         Intent share = new Intent(Intent.ACTION_SEND);
@@ -38,6 +58,9 @@ public class InstagramTest extends AppCompatActivity {
         // Broadcast the Intent.
         startActivity(Intent.createChooser(share, "Share to"));
     }
+
+    @Override
+    public void run() {
+        createInstagramIntent(type, mediaPath);
+    }
 }
-
-
