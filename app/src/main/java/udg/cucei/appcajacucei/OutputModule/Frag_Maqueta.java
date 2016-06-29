87e42a7ac8c21abd1b9e46d0ffa93557b759a957
edulisespz <ed_ulisespz@hotmail.com>
2016-06-29 18:51:08 -0500

    Input Module finished! working on the outputModule: -frag_maqueta - dinamic layout V1.5,    dinamic imputo layout ready "CORE"

--- app/src/main/java/udg/cucei/appcajacucei/OutputModule/Frag_Maqueta.java
+++ app/src/main/java/udg/cucei/appcajacucei/OutputModule/Frag_Maqueta.java
@@ -3,9 +3,11 @@ package udg.cucei.appcajacucei.OutputModule;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -14,6 +16,8 @@ import udg.cucei.appcajacucei.R;
 
 public class Frag_Maqueta extends Fragment {
 
+    int layTop_h;
+    int layTop_w;
 
     public Frag_Maqueta() {
         // Required empty public constructor
@@ -23,25 +27,52 @@ public class Frag_Maqueta extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        int x=0,y=0;
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_frag__maqueta, container, false);
 
 
         //LinearLayOut Setup
-        LinearLayout linearLayout=(LinearLayout) rootView.findViewById(R.id.LinLayTopView);
+        final LinearLayout linearLayout=(LinearLayout) rootView.findViewById(R.id.LinLayTopView);
 
-        LinearLayout linearLayout_line[]= new LinearLayout[4];
+        ViewTreeObserver observer = linearLayout.getViewTreeObserver();
+            observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    layTop_h=linearLayout.getHeight();
+                    layTop_w=linearLayout.getWidth();
+                    Log.d("params","H= "+Integer.toString(layTop_h) +" W= "+Integer.toString(layTop_w)  );
+
+                    setNumberOfItemsOnTOPLayout(linearLayout, 65, 22);// CORE
+
+                    linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                }
+            });
+
+        //setNumberOfItemsOnTOPLayout(linearLayout, 50, 50);
+
+
+
+
+        return rootView;
+    }
+
+
+
+    private void setNumberOfItemsOnTOPLayout(LinearLayout linearLayout, int item_H,int item_W){
+
+        int rows=1,columns=1;
+
+        LinearLayout linearLayout_line;
         ImageView imageView;
-        while (x<4){
-            linearLayout_line[x]= new LinearLayout(getActivity());
-            linearLayout_line[x].setOrientation(LinearLayout.HORIZONTAL);
+        while (rows*item_H < layTop_h){//adds rows
+            linearLayout_line= new LinearLayout(getActivity());
+            linearLayout_line.setOrientation(LinearLayout.HORIZONTAL);
 
-            //linearLayout_line[x].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-            //        LinearLayout.LayoutParams.WRAP_CONTENT));
+            columns=1;
+            while ( columns*item_W < layTop_w) {//adds columns
 
-            y=0;
-            while ( y < 3) {
                 //ImageView Setup
                 imageView = new ImageView(getActivity());
 
@@ -49,39 +80,22 @@ public class Frag_Maqueta extends Fragment {
                 imageView.setImageResource(R.drawable.frag_maque_latasuperior);
 
                 //setting image position
-                imageView.setLayoutParams(new LinearLayout.LayoutParams(75, 25) );
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(item_W, item_H) );
                 imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
                 //adding view to layout
-                linearLayout_line[x].addView(imageView);
-                y++;
+                linearLayout_line.addView(imageView);
+                columns++;
             }
 
-            linearLayout.addView(linearLayout_line[x], new ViewGroup.LayoutParams(
-                                                            ViewGroup.LayoutParams.WRAP_CONTENT,
-                                                             ViewGroup.LayoutParams.WRAP_CONTENT)
-                                                    );
+            linearLayout.addView(linearLayout_line, new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            );
 
-            x++;
+            rows++;
         }
-
-
-
-        //linearLayout.setOrientation(LinearLayout.VERTICAL);
-        //
-        //linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-        //        LinearLayout.LayoutParams.MATCH_PARENT));
-
-
-
-
-
-
-
-
-
-
-        return rootView;
+        Log.d("column&Rows","C= "+Integer.toString(columns-1) +" R= "+Integer.toString(rows-1)  );
     }
 
 }
