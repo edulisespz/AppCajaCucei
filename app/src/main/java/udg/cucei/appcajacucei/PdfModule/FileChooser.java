bf4e5ff074a5d8a4f15338ab05f1a39c25ad2db1
edulisespz <ed_ulisespz@hotmail.com>
2016-07-20 00:33:37 -0500

    file sistem for sharing -unfinished V 0.5 -changed logo icon(my crap)

--- app/src/main/java/udg/cucei/appcajacucei/PdfModule/FileChooser.java
+++ app/src/main/java/udg/cucei/appcajacucei/PdfModule/FileChooser.java
@@ -26,11 +26,7 @@ public class FileChooser extends AppCompatActivity {
 
         final ListView listview = (ListView) findViewById(R.id.listViewFiles);
 
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
+        final ArrayList<String> list = new ArrayList<String>();
 
 
         String path = Environment.getExternalStorageDirectory().toString()+"/pdfdemo_itext";
@@ -38,17 +34,17 @@ public class FileChooser extends AppCompatActivity {
         File f = new File(path);
         File file[] = f.listFiles();
         Log.d("Files", "Size: "+ file.length);
-        for (int i=0; i < file.length; i++) //TODO COSASSASSSS, cansado ahorita
+
+        for (int i=0; i < file.length; i++)
         {
             Log.d("Files", "FileName:" + file[i].getName());
+
+            list.add(file[i].getName() );
+
         }
 
 
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
         final StableArrayAdapter adapter = new StableArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
@@ -58,16 +54,8 @@ public class FileChooser extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
+
+                //TODO: do smoething here
             }
 
         });
